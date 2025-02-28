e0d3d01cb8d05a3786ac51cf046459f41059c358
Juan Sebastian <sebastianobh@gmail.com>
2025-02-28 15:06:53 -0500

    fix: :ambulance: Sync: Arreglo de Tailwind (final)
    Arreglo de bugs en el codigo que estaban contenidos en los modules
    
    La versión de Tailwind que estaba manejando anteriormente no dejaba trabajar con la carpeta aparte de postcss, por lo que borré la dependencia @tailwindcss/postcss, y cambié versiones, y ya funciona todo correctamente.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/app/**/*.{js,jsx,ts,tsx}', // Incluye todos los archivos en la carpeta app y sus subcarpetas
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+      extend: {
+        backgroundImage: {
+          'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+          'gradient-conic':
+            'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        },
+      },
+    },
+    plugins: [],
+  }
