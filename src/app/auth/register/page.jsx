3b2b51ca9061925203d7d3680eb85ebec9ad231c
Juan Sebastian <sebastianobh@gmail.com>
2025-03-13 08:41:45 -0500

    fix: :zap: Codigo listo, probado, completado, sin errores
    Había un error en la configuración del tailwind config que no permitía aplicar los estilos al navbar, + el error del secret ya se solucionó, y se hicieron todas las pruebas respectivas
    
    Faltó prueba de despliegue

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -37,7 +37,7 @@ function RegisterPage() {
   return (
     <div className="h-[calc(100vh-7rem)] flex justify-center items-center">
       <form onSubmit={onSubmit} className="w-1/4">
-        <h1 className="text-slate-200 font-bold text-4xl mb-4">Register</h1>
+        <h1 className="text-slate-200 font-bold flex justify-center text-4xl mb-4">Register</h1>
 
         <label htmlFor="username" className="text-slate-500 mb-2 block text-sm">
           Username:
