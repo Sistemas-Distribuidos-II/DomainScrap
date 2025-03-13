ede80703fe8344bd985315b9b640b4dea63e943d
Juan Sebastian <sebastianobh@gmail.com>
2025-03-13 00:45:50 -0500

    feat: :sparkles: Bcrypt, Hash, Comparativa de Datos
    Los datos se comparan con los ya guardados en la abse de datos para que no se repitan los nombres de usuario y emails. Las contraseñas se devuelven en formato Hash //hashedPassword con bcrypt
    
    Pendiente JWT

--- src/app/backend/api/auth/register/route.js
+++ src/app/backend/api/auth/register/route.js
@@ -1,12 +1,46 @@
 import { NextResponse } from 'next/server'
+import bcrypt from 'bcryptjs'   
+import jwt from 'jsonwebtoken'
 import db from '@/config/db'
 
 export async function POST (request) {
     const data = await request.json();
 
-    console.log(data);
+    const userFound = await db.user.findUnique({
+        where: {
+            email: data.email
+        }
+    })
+
+    if (userFound) {
+        return NextResponse.json({
+            message: 'Email already exists'
+        }, {
+            status: 400
+        })
+    }
+
+    const usernameFound = await db.user.findUnique({
+        where: {
+            username: data.username
+        }
+    })
+
+    if (usernameFound) {
+        return NextResponse.json({
+            message: 'Username already exists'
+        }, {
+            status: 400
+        })
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
     const newUser = await db.user.create({
-        data
+        data: {
+            username: data.username,
+            email: data.email,
+            password: hashedPassword
+        }
     })
 
     return NextResponse.json(newUser);
