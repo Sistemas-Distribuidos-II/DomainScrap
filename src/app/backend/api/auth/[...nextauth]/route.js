5a57d72e15ef26591daf234423d7145580df9c04
Juan Sebastian <sebastianobh@gmail.com>
2025-03-13 05:32:31 -0500

    build: :sparkles: Frontend Login + Dashboard /frontend/pages/auth/ login y pages/dashboard, + Middleware
    Ya quedó el front del Login page, sin redirección para que no se vaya a la ruta de nextauth sino a la dashboard apenas se validen los datos del login, se movió la carpeta middleware y ya se creó el archivo para proteger las rutas
    
    Queda el config del dashboard y desarrollar el middleware

--- src/app/backend/api/auth/[...nextauth]/route.js
+++ src/app/backend/api/auth/[...nextauth]/route.js
@@ -4,40 +4,48 @@ import db from "@/config/db";
 import bcrypt from "bcryptjs";
 
 const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Sistemas-Distribuidos-II",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "********",
+        },
+      },
+      async authorize(credentials, req) {
+        console.log(credentials);
 
-    providers: [
-        CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-                email: { label: "Email", type: "email", placeholder: "Sistemas-Distribuidos-II" },
-                password: { label: "Password", type: "password", placeholder: "********" }
-            },
-            async authorize(credentials, req) {
-                console.log(credentials)
+        const userFound = await db.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
 
-                const userFound = await db.user.findUnique({
-                    where: {
-                        email: credentials.email
-                    }
-                })
+        if (!userFound) return null;
 
-                if(!userFound) return null
+        console.log(userFound);
 
-                console.log(userFound)
+        const matchPassword = await bcrypt.compare(
+          credentials.password,
+          userFound.password
+        );
+        if (!matchPassword) return null;
 
-                const matchPassword = await bcrypt.compare(credentials.password, userFound.password) 
-                if (!matchPassword) return null
-
-                return {
-                    id: userFound.id,
-                    name: userFound.username,
-                    email: userFound.email,
-
-                }
-
-            }, 
-        }),
-    ],
+        return {
+          id: userFound.id,
+          name: userFound.username,
+          email: userFound.email,
+        };
+      },
+    }),
+  ],
 };
 
 //Ejecución /backend/api/auth/signin
--- src/app/frontend/pages/auth/register/page.jsx
+++ src/app/frontend/pages/auth/register/page.jsx
@@ -2,18 +2,20 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
+import { signIn } from 'next-auth/react';
+import { useState } from 'react';
 
 function RegisterPage() {
 
     const {
         register,
         handleSubmit,
-        formState: {errors},
+        formState: { errors },
     } = useForm();
     const router = useRouter()
 
     const onSubmit = handleSubmit(async (data) => {
-        
+
         if (data.password !== data.confirmPassword) {
             return alert('Passwords do not match');
         }
@@ -39,71 +41,83 @@ function RegisterPage() {
 
     console.log(errors);
 
-  return (
-    <div className='h-[calc(100vh-7rem)] flex items-center justify-center'>
+    return (
+        <div className='h-[calc(100vh-7rem)] flex items-center justify-center'>
 
-        <form onSubmit={onSubmit} className='w-1/4'>
+            <form onSubmit={onSubmit} className='w-1/4'>
 
-          <h1 className='text-slate-200 font-bold text-4xl mb-4 flex justify-center'>
-            Register
-            </h1>
+                <h1 className='text-slate-200 font-bold text-4xl mb-4 flex justify-center'>
+                    Register
+                </h1>
 
-            <label htmlFor="username" className='text-slate-500 mb-2 block text-sm'>Username:</label>
-            <input type="text" {...register ("username", {required: {
-                value: true, message: "This field is required"},})}
-            className='p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full'
-            placeholder='Username123'
-            />
-            {
-                errors.username && (
-                    <span className='text-red-500 text-xs'>{errors.username.message}</span>
-                )
-            }
+                <label htmlFor="username" className='text-slate-500 mb-2 block text-sm'>Username:</label>
+                <input type="text" {...register("username", {
+                    required: {
+                        value: true, message: "This field is required"
+                    },
+                })}
+                    className='p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full'
+                    placeholder='Username123'
+                />
+                {
+                    errors.username && (
+                        <span className='text-red-500 text-xs'>{errors.username.message}</span>
+                    )
+                }
 
-            <label htmlFor="email" className='text-slate-500 mb-2 block text-sm'>Email:</label>
-            <input type="email" {...register ("email", {required: {
-                value: true, message: "This field is required"},})}
-            className='p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full'
-            placeholder='youremail@mail.com'
-            />
-            {
-                errors.email && (
-                    <span className='text-red-500 text-xs'>{errors.email.message}</span>
-                )
-            }
+                <label htmlFor="email" className='text-slate-500 mb-2 block text-sm'>Email:</label>
+                <input type="email" {...register("email", {
+                    required: {
+                        value: true, message: "This field is required"
+                    },
+                })}
+                    className='p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full'
+                    placeholder='youremail@mail.com'
+                />
+                {
+                    errors.email && (
+                        <span className='text-red-500 text-xs'>{errors.email.message}</span>
+                    )
+                }
 
-            <label htmlFor="password" className='text-slate-500 mb-2 block text-sm'>Password:</label>
-            <input type="password" {...register("password", {required: {
-                value: true, message: "This field is required"},})}
-            className='p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full'
-            placeholder='**********'
-            />
-            {
-                errors.password && (
-                    <span className='text-red-500 text-xs'>{errors.password.message}</span>
-                )
-            }
+                <label htmlFor="password" className='text-slate-500 mb-2 block text-sm'>Password:</label>
+                <input type="password" {...register("password", {
+                    required: {
+                        value: true, message: "This field is required"
+                    },
+                })}
+                    className='p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full'
+                    placeholder='**********'
+                />
+                {
+                    errors.password && (
+                        <span className='text-red-500 text-xs'>{errors.password.message}</span>
+                    )
+                }
 
-            <label htmlFor="confirmPassword" className='text-slate-500 mb-2 block text-sm'>Confirm Password:</label>
-            <input type="password" {...register("confirmPassword", {required: {
-                value: true, message: "This field is required"},})}
-            className='p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full'
-            placeholder='**********'
-            />
-            {
-                errors.confirmPassword && (
-                    <span className='text-red-500 text-xs'>{errors.confirmPassword.message}</span>
-                )
-            }
+                <label htmlFor="confirmPassword" className='text-slate-500 mb-2 block text-sm'>Confirm Password:</label>
+                <input type="password" {...register("confirmPassword", {
+                    required: {
+                        value: true, message: "This field is required"
+                    },
+                })}
+                    className='p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full'
+                    placeholder='**********'
+                />
+                {
+                    errors.confirmPassword && (
+                        <span className='text-red-500 text-xs'>{errors.confirmPassword.message}</span>
+                    )
+                }
 
-            <button
-                className='w-full bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-pink-500 hover:to-orange-500 text-white p-3 rounded-lg mt-2'
-            >Register</button>
+                <button
+                    className='w-full bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-pink-500 hover:to-orange-500 text-white p-3 rounded-lg mt-2'
+                >Register</button>
 
-        </form> 
+            </form>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default RegisterPage
